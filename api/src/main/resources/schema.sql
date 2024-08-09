4f5f6eb32c7a73c8a4cbd354d67e3aab52313a08
Andrew T <me@andrewtran-shi.dev>
2024-08-09 00:27:52 -0700

    modified:   api/src/main/resources/schema.sql

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -1,39 +1,35 @@
--- Drop tables if they exist
 DROP TABLE IF EXISTS orders;
+
 DROP TABLE IF EXISTS menu;
-DROP TABLE IF EXISTS items;
+
 DROP TABLE IF EXISTS users;
 
--- Create tables again
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(100) DEFAULT 'customer' NOT NULL,
-    create_at TIMESTAMP DEFAULT NOW(),
-    update_at TIMESTAMP DEFAULT NOW()
-);
+CREATE TABLE
+    users (
+        user_id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(100) DEFAULT 'customer' NOT NULL,
+        create_at TIMESTAMP DEFAULT NOW(),
+        update_at TIMESTAMP DEFAULT NOW()
+    );
 
-CREATE TABLE items (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE
-);
+CREATE TABLE
+    menu (
+        menu_id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        type VARCHAR(100),
+        price NUMERIC(3, 2),
+        description VARCHAR(255),
+        create_at TIMESTAMP DEFAULT NOW(),
+        update_at TIMESTAMP DEFAULT NOW()
+    );
 
-CREATE TABLE menu (
-    menu_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    type VARCHAR(100),
-    price NUMERIC(3, 2),
-    description VARCHAR(255),
-    create_at TIMESTAMP DEFAULT NOW(),
-    update_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE orders (
-    user_id INT REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    menu_id INT REFERENCES menu(menu_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    create_at TIMESTAMP DEFAULT NOW(),
-    update_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE (user_id, menu_id)
-);
+CREATE TABLE
+    orders (
+        user_id INT REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        menu_id INT REFERENCES menu (menu_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        create_at TIMESTAMP DEFAULT NOW(),
+        update_at TIMESTAMP DEFAULT NOW(),
+        UNIQUE (user_id, menu_id)
+    );
