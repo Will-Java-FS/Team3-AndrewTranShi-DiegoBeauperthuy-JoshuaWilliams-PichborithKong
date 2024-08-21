e48417b5fbd30d7cde2cefda8cb46b453aba38df
Pichborith Kong <pichborith@gmail.com>
2024-08-21 07:43:08 -0700

    clean up unused code

--- api/src/main/java/com/revature/repository/OrderRepository.java
+++ api/src/main/java/com/revature/repository/OrderRepository.java
@@ -1,5 +1,4 @@
 package com.revature.repository;
-import com.revature.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
