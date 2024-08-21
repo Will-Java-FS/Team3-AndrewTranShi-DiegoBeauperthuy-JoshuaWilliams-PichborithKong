cba793af9c01b807b5abd75bd4b00940863622c5
Andrew T <me@andrewtran-shi.dev>
2024-08-21 14:35:01 -0700

    modified:   frontend/src/Components/Navbar.jsx 	modified:   frontend/src/Pages/Home.jsx 	modified:   frontend/src/pages/Button.jsx

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.jsx
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../Util/AuthContext"; // Import useAuth hook
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,19 +5,23 @@ import About from "../components/About";
 const Home = () => {
 	return (
 		<>
-			<div className="min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('/images/home-background.jpg')] bg-cover bg-no-repeat">
-				<div className="w-full lg:w-2/3 space-y-5">
-					<h1 className="text-backgroundColor font-semibold text-6xl">
+			<div className="min-h-screen flex flex-col justify-center items-center lg:flex-row lg:justify-between lg:px-32 px-5 bg-[url('/images/home-background.jpg')] bg-cover bg-no-repeat bg-center">
+				<div className="w-full lg:w-2/3 space-y-5 text-center lg:text-left lg:space-y-6">
+					<h1 className="text-backgroundColor font-semibold text-4xl lg:text-6xl">
 						The Best Food You Will Ever Try.
 					</h1>
-					<p className="text-backgroundColor">
+					<p className="text-backgroundColor text-base lg:text-lg">
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
 						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
 						aliquip ex ea commodo consequat.
 					</p>
-					<div className="lg:pl-44">
-						<Button title="Order Now" href="" />
+					<div className="mt-4 lg:mt-6 flex justify-center lg:justify-start">
+						<Button
+							title="Order Now"
+							href="/menu"
+							className="py-3 px-6 text-lg lg:text-xl" // Adjust size here
+						/>
 					</div>
 				</div>
 			</div>
